7b8a7b69485ddf9e36ccf9d6fe17a1e2a6e3de57
penguin5555 <aaravdeshmane@gmail.com>
2024-01-27 18:01:11 -0700

    Added temp eval func

--- ultimateMover.py
+++ ultimateMover.py
@@ -7,9 +7,28 @@ class Player:
     def __init__(self, board, color, t):
         self.color = color
     
-    def evaluationFunction(self, board):
-        # fill this part out
-        return 5
+    def evaluationFunction(self, board, color):
+        pieces = [
+            (chess.KING, 20_000), 
+            (chess.QUEEN, 900), 
+            (chess.ROOK, 500),
+            (chess.BISHOP, 330),
+            (chess.KNIGHT, 320),
+            (chess.PAWN, 100), 
+        ]
+
+        def pieceCount(piece, color):
+            # get amt of pieces on the board that are a certain color and type
+            return len(board.pieces(piece, color))
+
+        material = 0
+        for piece, value in pieces:
+            # in chess module, white is True, black is False
+            # therefore, to get the next agent, just do not agent
+            # True -> False, False -> True
+            material += value * (pieceCount(piece, color)-pieceCount(piece, not color))
+
+        return material
 
     def move(self, board, t):
         AGENTS = 2
@@ -40,7 +59,7 @@ class Player:
         # standard minimax code
         def evaluate(state, depth, agent):
             if depth/AGENTS == wantedDepth:
-                return self.evaluationFunction(state)
+                return self.evaluationFunction(state, agent)
             else:
                 if agent == self.color:
                     return maxVal(state, depth+1, agent)
@@ -73,5 +92,7 @@ class Player:
             # start with the next agent, as you are starting 1 depth lower in the tree
             vals.append(evaluate(successor, 1, getNextAgent(self.color)))
         print(vals)
+        print(board)
+        print(self.color)
         actions = list(board.legal_moves)
         return actions[vals.index(max(vals))]
